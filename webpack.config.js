17d9d40abaf292628ed269adbf3899310d732db4
Anthony PENA <pena.a@sfeir.com>
2022-07-21 09:37:18 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'gladiaio-sdk.js',
+    library: "gladiaio_sdk",
+  },
+};
