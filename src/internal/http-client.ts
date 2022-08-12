8b85dc00798fb00249fdd184b72653bb7ea0766f
Anthony PENA <pena.a@sfeir.com>
2022-08-12 11:55:20 +0200

    feat: can enforce fetch usage (browser only)

--- src/internal/http-client.ts
+++ src/internal/http-client.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import axiosFetchAdapter from '@vespaiach/axios-fetch-adapter';
 import { SDK_VERSION } from '../meta/sdk-version';
 import { GladiaClientParams } from '../client/gladia-client-params';
 
@@ -17,6 +18,7 @@ export interface HttpClient {
 export interface HttpClientFactoryParams {
   baseHeaders: Record<string, string | number | boolean>;
   baseUrl: string;
+  useFetch: boolean;
 }
 
 export type HttpClientFactory = (params: HttpClientFactoryParams) => HttpClient;
@@ -28,10 +30,14 @@ export function getHttpClient(params: GladiaClientParams) {
     'x-gladia-sdk': SDK_VERSION,
     'Content-Type': 'application/json',
   };
-  return factory({ baseHeaders, baseUrl: params.baseUrl ?? 'https://v2-api.gladia.io' });
+  return factory({
+    baseHeaders,
+    baseUrl: params.baseUrl ?? 'https://v2-api.gladia.io',
+    useFetch: params.useFetch ?? false,
+  });
 }
 
-const AxiosHttpClient: HttpClientFactory = ({ baseHeaders, baseUrl }) => {
+const AxiosHttpClient: HttpClientFactory = ({ baseHeaders, baseUrl, useFetch }) => {
   return {
     post(params) {
       const headers = { ...baseHeaders, ...params.headers };
@@ -40,8 +46,14 @@ const AxiosHttpClient: HttpClientFactory = ({ baseHeaders, baseUrl }) => {
         url.searchParams.set(key, String(value));
       }
       const responseType = params.responseType ?? 'json';
+      const adapter = (() => {
+        if (useFetch) {
+          return { adapter: axiosFetchAdapter };
+        }
+        return {};
+      })();
       return axios
-        .post(url.toString(), params.body, { headers, responseType })
+        .post(url.toString(), params.body, { headers, responseType, ...adapter })
         .then((response) => response.data);
     },
   };
