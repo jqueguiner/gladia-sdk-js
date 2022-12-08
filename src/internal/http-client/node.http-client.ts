10d61bee41001f905a50dece954187e5677a5af4
Anthony PENA <pena.a@sfeir.com>
2022-12-08 17:58:04 +0100

    feat: add header to know which http client is used

--- src/internal/http-client/node.http-client.ts
+++ src/internal/http-client/node.http-client.ts
@@ -6,7 +6,11 @@ import https from 'https';
 export const NodeHttpClient: HttpClientFactory = ({ baseHeaders, baseUrl, httpClientTimeout }) => {
   return {
     async post(params) {
-      const headers = headerSerializer({ ...baseHeaders, ...params.headers });
+      const headers = headerSerializer({
+        ...baseHeaders,
+        ...params.headers,
+        'x-gladia-sdk-http-client': 'node',
+      });
       const url = `${baseUrl}${params.url}${searchQueryParamSerializer(params.query)}`;
       const responseType = params.responseType ?? 'json';
       const timeout = httpClientTimeout ?? 300_000;
